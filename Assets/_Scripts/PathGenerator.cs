24e8e396abd998faecb1082f2c788673551502b8
bhadrachem <bhc9av@virginia.edu>
2018-09-05 14:14:08 +0530

    fixing path gen

---
+++ Assets/_Scripts/PathGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathGenerator : MonoBehaviour {
+
+    public GameObject[] PathArray;
+    public GameObject Player;
+    public List<GameObject> paths;
+    private float triggerDistance;
+    private float pointDistance;
+
+	// Use this for initialization
+	void Start () {
+
+        triggerDistance = 0;
+        pointDistance = 140+75;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Player.transform.position.z >= triggerDistance)
+        {
+            CreateNewPath();
+        }
+	}
+
+    // Late Update is called after Update
+    private void LateUpdate()
+    {
+        DeleteOldPaths();
+    }
+
+    // TODO: Implement. Will initialize the first 30 some paths so that player will 
+    // not be able to see path generation happening.
+    private void InitializePaths()
+    {
+
+    }
+
+    // Generates a new path and places it on the screen
+    private void CreateNewPath()
+    {
+        // instantiates path at Vector (0, 0, pointDistance)
+        GameObject path = Instantiate(GetRandomPath(), new Vector3(0, 0, pointDistance), Quaternion.identity);
+        // adds the path to the array of paths
+        paths.Add(path);
+        // resets the trigger distance. Need to change this once path init is implemented
+        triggerDistance = path.transform.position.z;
+        // resets point distance for next path
+        pointDistance += path.transform.localScale.z * 10;
+
+    }
+
+    // finds a random int i in range of number of paths and returns that index Path
+    private GameObject GetRandomPath()
+    {
+        int i = Random.Range(0, PathArray.Length);
+        return PathArray[i];
+
+    }
+
+    // Deletes old created paths whose z position is behind the player
+    private void DeleteOldPaths()
+    {
+        foreach (var path in paths)
+        {
+            // finds difference in z position
+            float zDiff = Player.transform.position.z - path.transform.position.z;
+            if (zDiff > 300)
+            {
+                paths.Remove(path);
+            }
+        }
+    }
+}
--- Assets/_Scripts/UnicornController.cs
+++ Assets/_Scripts/UnicornController.cs
@@ -1,21 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class UnicornController : MonoBehaviour {
 
     public Transform player;
+    public Collider uniCollider;
+    public float jumpVal;
+    public float dashVal;
     public float forwardSpeed;
+    public float angleSensitivity;
+    public RaycastHit hit;
+    public Animator uniAnim;
 
     private Camera mainCamera;
-    private Vector3 forwardVector;
+    private bool hasJumpedOnce = false;
+    private bool hasJumpedTwice = false;
+    private bool hasDashed = true;
+    private bool canJumpAgain = true;
 
     // Use this for initialization
     void Start ()
     {
 
         mainCamera = Camera.main;
-        forwardVector = new Vector3(0f, 0f, forwardSpeed);
 
     }
 	
@@ -23,33 +29,115 @@ public class UnicornController : MonoBehaviour {
 	void Update ()
     {
 
-        this.transform.position += forwardVector;
-
-        // camera moves in X/-X direction depending on head turn
-        Vector3 hmovement = GetMoveSpeed(mainCamera.transform.rotation.y);
-        this.transform.position += hmovement/3;
-
+        // moves playerbox in direction camera is facing
+        this.transform.Translate(mainCamera.transform.forward * Time.deltaTime * forwardSpeed, Space.World);
     }
 
     void LateUpdate()
     {
 
         // unicorn avatar follows main camera movements when turning head
-        float yVal = mainCamera.transform.eulerAngles.y;
-        player.eulerAngles = new Vector3(0, yVal, 0);
 
+        //TODO: Something going wrong, need to check with headset config
+        float yVal = mainCamera.transform.rotation.y;
+        player.transform.Rotate(new Vector3(0, yVal, 0));
+        //player.rotation.Set(0, yVal, 0, 0);
+        //player.eulerAngles = new Vector3(0, yVal, 0);
+
+        animControl(JumpCheck(), DashCheck());
     }
 
-    // calculates how far turning head moves you in that direction
-    private Vector3 GetMoveSpeed(float y)
+    // monitors jump controls
+    /* if player is touching the ground, he can jump
+        he can jump in mid air one time, if head rotation has come almost back to center
+        after that he must be touching the ground again to jump 
+        returns true if jump occurred. else false.*/
+    private bool JumpCheck()
     {
-        float xMove = Mathf.Min(Mathf.Abs(y * 10), 3);
 
-        if (y < 0)
+        float camRot = mainCamera.transform.rotation.x;
+        bool jumped = false;
+
+        if (camRot < -1*angleSensitivity)
         {
-            xMove *= -1;
+            if (canJumpAgain)
+            {
+                this.transform.position += new Vector3(0f, jumpVal, 0f);
+
+                jumped = true;
+                canJumpAgain = false;
+
+                if (!hasJumpedOnce)
+                {
+                    hasJumpedOnce = true;
+                }
+                else if (hasJumpedOnce && !hasJumpedTwice)
+                {
+                    hasJumpedTwice = true;
+                }
+                else
+                {
+                    Debug.Log("Error, booleans are wrong.");
+                }
+            }
+            else
+            {
+                if (Physics.Raycast(transform.position, -Vector3.up, 0.5f))
+                {
+                    hasJumpedTwice = false;
+                    this.transform.position += new Vector3(0f, jumpVal, 0f);
+
+                    jumped = true;
+                    canJumpAgain = false;
+                    hasJumpedOnce = true;
+                }
+            }
         }
 
-        return new Vector3(xMove, 0f, 0f);
+        if (camRot >= -0.15 && !hasJumpedTwice && !canJumpAgain)
+        {
+            canJumpAgain = true;
+        }
+
+        return jumped;
     }
+
+    // monitors dash controls
+    /* if player looks down he can dash
+        he can dash infinitely but not in a row. he must come back to a center head position
+        before he can dash again.
+        returns true if dash occurred. else false.*/
+    private bool DashCheck()
+    {
+        bool dashed = false;
+        float camRot = mainCamera.transform.rotation.x;
+
+        if (camRot > angleSensitivity && !hasDashed)
+        {
+            this.transform.position += new Vector3(0f, 0f, dashVal);
+            hasDashed = true;
+            dashed = true;
+        }
+
+        if (camRot <= 0.15)
+        {
+            hasDashed = false;
+        }
+
+        return dashed;
+    }
+
+    // applies animation based on whether a jump or dash occured.
+    private void animControl(bool jumpCheck, bool dashCheck)
+    {
+        if (jumpCheck)
+        {
+            uniAnim.SetTrigger("Jump");
+        }
+        if (dashCheck)
+        {
+            uniAnim.SetTrigger("Dash");
+        }
+    }
+
 }
